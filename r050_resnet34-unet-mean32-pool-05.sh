88540ae60c1b3d04f8aa2894f7e0b324186a6c4c
hengck23 <hengcherkeng235@gmail.com>
2023-07-12 07:26:17 +0800

    Add files via upload

---
+++ r050_resnet34-unet-mean32-pool-05.sh
@@ -0,0 +1,10 @@
+#fold1
+python src/r050_resnet34-unet-mean32-pool-05/run_train.py config_fold1_stage1_0
+python src/r050_resnet34-unet-mean32-pool-05/run_train.py config_fold1_stage1_1
+python src/r050_resnet34-unet-mean32-pool-05/run_train.py config_fold1_stage1_2
+python src/r050_resnet34-unet-mean32-pool-05/run_train.py config_fold1_stage2_0
+
+#fold2aa
+python src/r050_resnet34-unet-mean32-pool-05/run_train.py config_fold2aa_stage1_0
+python src/r050_resnet34-unet-mean32-pool-05/run_train.py config_fold2aa_stage1_2
+python src/r050_resnet34-unet-mean32-pool-05/run_train.py config_fold2aa_stage2_0
---
+++ r090-pvtv2-daformer-pool-02a.sh
@@ -0,0 +1,8 @@
+#fold1
+python src/r090-pvtv2-daformer-pool-02a/run_train.py config_fold1_stage1_0
+python src/r090-pvtv2-daformer-pool-02a/run_train.py config_fold1_stage1_1
+python src/r090-pvtv2-daformer-pool-02a/run_train.py config_fold1_stage2_0
+
+#fold2aa
+python src/r090-pvtv2-daformer-pool-02a/run_train.py config_fold2aa_stage1_0
+python src/r090-pvtv2-daformer-pool-02a/run_train.py config_fold2aa_stage2_0
